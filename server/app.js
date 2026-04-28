ddf17013241e0ac945aab1561729e15e5f3d4293
He0he0 <105949611+He0he0@users.noreply.github.com>
2026-04-28 15:26:53 -0700

    trouble shooting

--- server/app.js
+++ server/app.js
@@ -9,20 +9,18 @@ const userRoutes = require("./routes/users");
 
 const app = express();
 
-app.use(cors({ origin: process.env.CLIENT_URL || "http://localhost:5173" }));
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+app.options("*", cors());
 app.use(express.json());
 
 
 // Source - https://stackoverflow.com/a/55897146
 // Posted by Shohel, modified by community. See post 'Timeline' for change history
 // Retrieved 2026-04-28, License - CC BY-SA 4.0
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/planner", plannerRoutes);
