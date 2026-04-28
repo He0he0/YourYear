1bb69e9639fcbf6b21889bf9d93b4211a4981bd1
He0he0 <105949611+He0he0@users.noreply.github.com>
2026-04-28 15:20:02 -0700

    allowed cors access

--- server/app.js
+++ server/app.js
@@ -12,6 +12,18 @@ const app = express();
 app.use(cors({ origin: process.env.CLIENT_URL || "http://localhost:5173" }));
 app.use(express.json());
 
+
+// Source - https://stackoverflow.com/a/55897146
+// Posted by Shohel, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-04-28, License - CC BY-SA 4.0
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use("/api/auth", authRoutes);
 app.use("/api/planner", plannerRoutes);
 app.use("/api/users", userRoutes);
